6e0b06e52ae08cc7c82fc20d0d3f945671238810
koyeonwoo9380 <145740606+koyeonwoo9380@users.noreply.github.com>
2024-04-29 16:52:48 +0900

    Add files via upload

--- 이상한 문자 만들기.py
+++ 이상한 문자 만들기.py
@@ -1,15 +1,11 @@
 def solution(s):
-    k = 0
     answer = ""
-    for i in range(len(s)):
-        if s[k] == " ":
-            answer += " "
-            k += 1 
-        else:
-            if k % 2 == 0:
-                answer += s[k].upper()
-                k += 1 
+    word = s.split(" ")  # 띄어쓰기로 나눈다. 즉 단어마다 나눈다.
+    for i in word:   # for i in range(숫자) range 뒤에는 숫자가 나와야 해서 range안쓰고 word를 썼다. 
+        for j in range(len(i)):  # 단어의 글자 수만큼 반복
+            if j % 2 == 0:
+                answer += i[j].upper()
             else:
-                answer += s[k]
-                k += 1
-    return answer
+                answer += i[j].lower()
+        answer += " "       # for문이 끝나면 = 한 단어가 끝나면 " "띄어쓰기로
+    return answer[:-1]
