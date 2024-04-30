f40040b42588edb49bab259e7571285fa8297b5d
koyeonwoo9380 <145740606+koyeonwoo9380@users.noreply.github.com>
2024-04-30 11:22:50 +0900

    Add files via upload

---
+++ 숫자 문자열과 영단어.py
@@ -0,0 +1,29 @@
+def solution(s):
+    answer = ""
+    word = ""
+    for i in range(len(s)):
+        if s[i].isdigit():
+            answer += s[i]
+        else:
+            word += s[i]
+            if word == "one":
+                answer += '1'
+            elif word == "two":
+                answer += '2'
+            elif word == "three":
+                answer += str(3)
+            elif word == "four":
+                answer += '4'
+            elif word == "five":
+                answer += str(5)
+            elif word == "six":
+                answer += str(6)
+            elif word == "seven":
+                answer += '7'
+            elif word == "eight":
+                answer += '8'
+            elif word == "nine":
+                answer += str(9)
+            if word in ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]:
+                word = ""
+    return int(answer)
